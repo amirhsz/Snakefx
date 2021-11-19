5f7d7712cfe95451c54c04b443e78eaeb9b76bae
Amir <amir1386amir2006@gmail.com>
2021-11-19 20:25:45 +0330

    abstract piece implemented

---
+++ src/main/java/inc/faregh/snake/model/Piece.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inc.faregh.snake.model;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Amir
+ */
+public abstract class Piece {
+    Point pos;
+    String shapename;
+
+    public Piece(Point pos, String shapename) {
+        this.pos = pos;
+        this.shapename = shapename;
+    }
+
+    public Point getPos() {
+        return pos;
+    }
+
+    public String getShapename() {
+        return shapename;
+    }
+
+}
