5163ef1affa1ed307d1b85e6590a2014496979c6
Amir <amir1386amir2006@gmail.com>
2021-11-19 20:28:11 +0330

    class Fruit created

---
+++ src/main/java/inc/faregh/snake/model/Fruit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inc.faregh.snake.model;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Amir
+ */
+public class Fruit extends Piece {
+
+    public Fruit(Point pos, String shapename) {
+        super(pos, shapename);
+    }
+    
+}
