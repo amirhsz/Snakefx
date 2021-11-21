5f1e44eba5702c426a83e073be2c76cc4132363e
Amir <amir1386amir2006@gmail.com>
2021-11-21 18:19:08 +0330

    Piece redeigned

--- src/main/java/inc/faregh/snake/model/Pieces/Piece.java
+++ src/main/java/inc/faregh/snake/model/Pieces/Piece.java
@@ -1,10 +1,11 @@
+package inc.faregh.snake.model.Pieces;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inc.faregh.snake.model.Pieces;
 
+import inc.faregh.snake.model.Strategy.Strategy;
 import java.awt.Point;
 
 /**
@@ -12,12 +13,14 @@ import java.awt.Point;
  * @author Amir
  */
 public abstract class Piece {
-    Point pos;
-    String shapename;
 
-    public Piece(Point pos, String shapename) {
+    protected Point pos;
+    protected String shapename;
+    protected Strategy s;
+
+    public Piece(Point pos, Strategy s) {
         this.pos = pos;
-        this.shapename = shapename;
+        this.s = s;
     }
 
     public Point getPos() {
@@ -26,6 +29,15 @@ public abstract class Piece {
 
     public String getShapename() {
         return shapename;
+    }
+
+    public Strategy getStrategy(){
+        return s;
+    }
+
+    @Override
+    public String toString(){
+        return "a piece called: "+this.getClass().getSimpleName()+", in position: "+pos;
     }
 
 }
